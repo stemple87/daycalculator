ced2df4c7ab625b96db7c493f9caa555e98dda5d
simon chris <simonctemple@live.com>
2016-02-18 14:00:47 -0800

    first test working

--- Tests/DayCalculatorTest.cs
+++ Tests/DayCalculatorTest.cs
@@ -13,12 +13,41 @@ namespace DayCalculatorNS.Objects
       int input1 = 15;
       int input2 = 2;
       int input3 = 2016;
-
       DayCalculator testDayCalculator = new DayCalculator(input1, input2, input3);
-      //
+
       // //Act
       var result = testDayCalculator.DayName();
-      //
+
+      //Assert
+      Assert.Equal("Monday", result);
+    }
+    [Fact]
+    public void Test2_ToCheckTheDayName_ReturnThursday()
+    {
+      // //Arrange
+      int input1 = 18;
+      int input2 = 2;
+      int input3 = 2016;
+      DayCalculator testDayCalculator = new DayCalculator(input1, input2, input3);
+
+      // //Act
+      var result = testDayCalculator.DayName();
+
+      //Assert
+      Assert.Equal("Thursday", result);
+    }
+    [Fact]
+    public void Test1_ToCheckTheDayName_ReturnMonday()
+    {
+      // //Arrange
+      int input1 = 29;
+      int input2 = 2;
+      int input3 = 2016;
+      DayCalculator testDayCalculator = new DayCalculator(input1, input2, input3);
+
+      // //Act
+      var result = testDayCalculator.DayName();
+
       //Assert
       Assert.Equal("Monday", result);
     }
